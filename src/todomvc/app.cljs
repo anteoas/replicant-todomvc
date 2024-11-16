baeea93ef30b049f13fedb092e7fef460014451f
PEZ <pez@pezius.com>
2024-11-17 00:16:27 +0100

    Extracts db to its own namespace

--- src/todomvc/app.cljs
+++ src/todomvc/app.cljs
@@ -1,39 +1,27 @@
 (ns todomvc.app
   (:require [clojure.string :as string]
             [clojure.walk :as walk]
-            [cognitect.transit :as t]
             [gadget.inspector :as inspector]
             [replicant.dom :as r-dom]
             [todomvc.actions :as actions]
+            [todomvc.db :as db]
             [todomvc.router :as router]
-            [todomvc.util :as cu]
+            [todomvc.util :as util]
             [todomvc.views :as views]))
 
-(def ^:private default-db {:app/todo-items []
-                           :app/el nil})
+(defonce el (js/document.getElementById "app"))
 
-(defonce ^:private !state (atom nil))
-
-(def storage-key "replicant-todomvc")
-
-(def persist-keys [:app/todo-items
-                   :add/draft
-                   :app/mark-all-state])
-
-(defn- load-persisted! []
-  (or (->> (.getItem js/localStorage storage-key)
-           (t/read (t/reader :json)))
-      default-db))
-
-(defn- persist! [state]
-  (.setItem js/localStorage storage-key (t/write (t/writer :json) (select-keys state persist-keys))))
+(defn- render! [state]
+  (r-dom/render
+   el
+   (views/app-view state)))
 
 (defn- enrich-action-from-event [{:replicant/keys [js-event node]} actions]
   (walk/postwalk
    (fn [x]
      (if (keyword? x)
        (cond (= "event" (namespace x)) (let [path (string/split (name x) #"\.")]
-                                         (cu/js-get-in js-event path))
+                                         (util/js-get-in js-event path))
              (= :dom/node x) node
              :else x)
        x))
@@ -48,34 +36,22 @@
        :else x))
    action))
 
-(defonce el (js/document.getElementById "app"))
-
-(defn- render! [state]
-  (r-dom/render
-   el
-   (views/app-view state)))
-
 (defn- event-handler [replicant-data actions]
   (doseq [action actions]
     (prn "Triggered action" action)
     (let [enriched-action (->> action
                                (enrich-action-from-event replicant-data)
-                               (enrich-action-from-state @!state))]
+                               (enrich-action-from-state @db/!state))]
       (prn "Enriched action" enriched-action)
-      (actions/handle-action! !state replicant-data enriched-action))))
+      (actions/handle-action! db/!state replicant-data enriched-action))))
 
 (defn ^{:dev/after-load true :export true} start! []
-  (render! @!state))
+  (render! @db/!state))
 
 (defn ^:export init! []
-  (reset! !state (load-persisted!))
-  (inspector/inspect "App state" !state)
+  (db/init!)
+  (inspector/inspect "App state" db/!state)
   (r-dom/set-dispatch! event-handler)
   (router/start! router/routes event-handler)
-  (add-watch !state :persist (fn [_ _ old-state new-state]
-                               (when (not= old-state new-state)
-                                 (render! new-state)
-                                 (when (not= (select-keys old-state persist-keys)
-                                             (select-keys new-state persist-keys))
-                                   (persist! new-state)))))
+  (db/watch! render!)
   (start!))
---
+++ src/todomvc/db.cljs
@@ -0,0 +1,32 @@
+(ns todomvc.db
+  (:require [cognitect.transit :as t]))
+
+(defonce !state (atom nil))
+
+(def ^:private default-db {:app/todo-items []
+                           :app/el nil})
+
+(def ^:private storage-key "replicant-todomvc")
+
+(def ^:private persist-keys [:app/todo-items
+                             :add/draft
+                             :app/mark-all-state])
+
+(defn- load-persisted! []
+  (or (->> (.getItem js/localStorage storage-key)
+           (t/read (t/reader :json)))
+      default-db))
+
+(defn- persist! [state]
+  (.setItem js/localStorage storage-key (t/write (t/writer :json) (select-keys state persist-keys))))
+
+(defn init! []
+  (reset! !state (load-persisted!)))
+
+(defn watch! [render!]
+  (add-watch !state :persist (fn [_ _ old-state new-state]
+                               (when (not= old-state new-state)
+                                 (render! new-state)
+                                 (when (not= (select-keys old-state persist-keys)
+                                             (select-keys new-state persist-keys))
+                                   (persist! new-state))))))
