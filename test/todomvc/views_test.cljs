56be52342dfdcad6dca8da1e8a9a54886801c682
PEZ <PEZ@users.noreply.github.com>
2024-11-20 16:10:52 +0000

    deploy: d253c0bfd542a07e2f64418785db9cfa282a644e

--- test/todomvc/views_test.cljs
+++ test/todomvc/views_test.cljs
@@ -2,7 +2,7 @@
   (:require
    [clojure.test :refer [deftest is testing]]
    [lookup.core :as l]
-   [todomvc.hiccup-attributes :as ha]
+   [todomvc.actions :as a]
    [todomvc.views :as sut]))
 
 #_{:clj-kondo/ignore [:private-call]}
@@ -30,14 +30,128 @@
                              second
                              :item/title)))))
 
+(defn- select-attribute
+  [selector path data]
+  (let [elements (l/select selector data)]
+    (->> (keep (fn [element]
+                 (when (map? (second element))
+                   (get-in (second element) path)))
+               elements))))
+
+(defn- flatten-actions [actionss]
+  (reduce into [] actionss))
+
+(defn- select-actions [selector path data]
+  (->> (select-attribute selector path data)
+       flatten-actions))
+
+(defn- handle-actions [state replicant-data actions]
+  (reduce (fn [{state :new-state :as acc} action]
+            (let [{:keys [new-state effects]} (a/handle-action state replicant-data action)]
+              (cond-> acc
+                new-state (assoc :new-state new-state)
+                effects (update :effects into effects))))
+          {:new-state state
+           :effects []}
+          actions))
+
+#_{:clj-kondo/ignore [:private-call]}
+(defn test-add-view-mount [state]
+  #_{:clj-kondo/ignore [:inline-def :clojure-lsp/unused-public-var]}
+  (comment
+    (def state {}))
+  (let [on-mount-actions (->> (sut/add-view state)
+                              (select-actions :input.new-todo [:replicant/on-mount]))
+        {:keys [new-state effects] :as result} (handle-actions state
+                                                               {:replicant/node :input-dom-node}
+                                                               on-mount-actions)]
+    (is (= :input-dom-node
+           (:add/draft-input-element new-state)))
+    (is (empty? effects)
+        "it does so without other side-effects")
+    result))
+
+#_{:clj-kondo/ignore [:private-call]}
+(defn test-add-view-input [state add-text]
+  #_{:clj-kondo/ignore [:inline-def :clojure-lsp/unused-public-var]}
+  (comment
+    (def state {:add/draft-input-element :input-dom-node})
+    (def add-text "Input"))
+  (let [on-input-actions (->> (sut/add-view state)
+                              (select-actions :input.new-todo [:on :input]))
+        {:keys [new-state effects] :as result} (handle-actions state
+                                                               {:replicant/js-event (clj->js {:target {:value add-text}})}
+                                                               on-input-actions)]
+    (is (= add-text
+           (:add/draft new-state)))
+    (is (empty? effects)
+        "it does so without other side-effects")
+    result))
+
 #_{:clj-kondo/ignore [:private-call]}
 (deftest add-view
-  (testing "its form-submit has a prevent-default effect"
-    (is (some #{[:dom/ax.prevent-default]}
-              (->> (sut/add-view {:add/draft "New item"})
-                   (ha/select-attribute 'form [:on :submit])
-                   first
-                   set)))))
+  (testing "it saves draft input element on mount"
+    (let [{:keys [new-state]} (test-add-view-mount {})]
+
+      (testing "it updates the draft from the `.new-todo` input event"
+        (let [input-text "Input"
+              {:keys [new-state]} (test-add-view-input new-state input-text)]
+
+          (testing "it handles the form submit event"
+            (let [on-submit-actions (->> (sut/add-view new-state)
+                                         (select-actions :form [:on :submit]))
+                  {:keys [new-state effects]} (handle-actions new-state
+                                                              {}
+                                                              on-submit-actions)]
+              (is (= input-text
+                     (-> new-state :app/todo-items first :item/title))
+                  "it adds the new item to the todo items")
+              (is (= ""
+                     (:add/draft new-state))
+                  "it clears the draft")
+              (is (some #{[:dom/fx.prevent-default]}
+                        (set effects))
+                  "it prevents the default form submit action")
+              (is (some #{[:dom/fx.set-input-text :input-dom-node ""]}
+                        (set effects))
+                  "it clears the input element")))))
+
+      (testing "it trims the input"
+        (let [input-text "Input"
+              untrimmed-test (str "  " input-text "  ")
+              {:keys [new-state]} (test-add-view-input new-state untrimmed-test)]
+
+          (testing "it handles the form submit event"
+            (let [on-submit-actions (->> (sut/add-view new-state)
+                                         (select-actions :form [:on :submit]))
+                  {:keys [new-state]} (handle-actions new-state
+                                                      {}
+                                                      on-submit-actions)]
+              (is (= input-text
+                     (-> new-state :app/todo-items first :item/title))
+                  "it adds the new item with trimmed text to the todo items")))))
+
+      (testing "it doesn't add an item when the input is empty"
+        (let [input-text ""
+              {:keys [new-state]} (test-add-view-input new-state input-text)]
+
+          (testing "it handles the form submit event"
+            (let [on-submit-actions (->> (sut/add-view new-state)
+                                         (select-actions :form [:on :submit]))
+                  {:keys [new-state effects]} (handle-actions new-state
+                                                              {}
+                                                              on-submit-actions)]
+              (is (empty? (:app/todo-items new-state))
+                  "it does not add a new item to the todo items")
+              (is (= ""
+                     (:add/draft new-state))
+                  "it clears the draft")
+              (is (some #{[:dom/fx.prevent-default]}
+                        (set effects))
+                  "it prevents the default form submit action")
+              (is (some #{[:dom/fx.set-input-text :input-dom-node ""]}
+                        (set effects))
+                  "the input element remains blank"))))))))
 
 (deftest app-view
   (testing "it shows a `.todoapp` element"
@@ -49,13 +163,13 @@
 
   (testing "the `.todoapp` element contains an autofocused `.new-todo` input in the `.header` element"
     (is (= '(true)
-           (ha/select-attribute '[.todoapp .header input.new-todo]
+           (select-attribute '[.todoapp .header input.new-todo]
                                 [:autofocus]
                                 (sut/app-view {})))
         "with an empty app state")
 
     (is (= '(true)
-           (ha/select-attribute '[.todoapp .header input.new-todo]
+           (select-attribute '[.todoapp .header input.new-todo]
                                 [:autofocus]
                                 (sut/app-view {:app/todo-items [{:item/title "First item"}]})))
         "with items in the app state"))
@@ -72,10 +186,10 @@
     (is (seq (l/select '[.todoapp .footer] (sut/app-view {:app/todo-items [{:item/title "First item"}]})))
         "it has a `.footer` when there are items"))
 
-  (testing "all form-submits have a prevent-default effect"
+  (testing "all form-submits have a prevent-default action"
     (is (every? (partial some #{[:dom/ax.prevent-default]})
                 (->> (sut/app-view {:app/todo-items [{:item/title "First item"}]
                                     :edit/editing-item-index 0
                                     :app/item-filter :filter/all})
-                     (ha/select-attribute 'form [:on :submit])
+                     (select-attribute 'form [:on :submit])
                      (map set))))))
