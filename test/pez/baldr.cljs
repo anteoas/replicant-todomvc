7ddc0f5c8eb78514f134bc70e0f16ea81f500490
PEZ <pez@pezius.com>
2024-11-25 15:40:43 +0100

    Purify get-report

--- test/pez/baldr.cljs
+++ test/pez/baldr.cljs
@@ -25,23 +25,27 @@
 (defn- indent [level]
   (apply str (repeat (* 2 level) " ")))
 
-(defn- -report-test! [m env state {:keys [color bullet bullet-color]}]
+(defn- get-report [m env state {:keys [color bullet bullet-color]}]
   (let [seen-contexts (:contexts state)
-        message (or (:message m) (pr-str (:expected m)))
         contexts (:testing-contexts env)
-        common-prefix-length (count (take-while true? (map = (reverse seen-contexts)
-                                                           (reverse contexts))))
-        new-contexts (reverse (take (- (count contexts) common-prefix-length) contexts))]
-    (when (seq new-contexts)
-      (doseq [[idx ctx] (map-indexed vector new-contexts)]
-        (println (str (indent (+ common-prefix-length idx 2))
-                      (bold ctx)))))
-    (println (str (indent (+ 2 (count contexts)))
-                  (str (bullet-color bullet) " " (color message))))
-    (assoc state :contexts contexts)))
+        common-contexts (take-while true? (map = (reverse seen-contexts) (reverse contexts)))
+        common-prefix-length (count common-contexts)
+        new-contexts (reverse (take (- (count contexts) common-prefix-length) contexts))
+        message (or (:message m) (pr-str (:expected m)))]
+    {:new-state (assoc state :contexts contexts)
+     :printouts (cond-> []
+                  (seq new-contexts) (into (map-indexed (fn [idx ctx]
+                                                          (str (indent (+ common-prefix-length idx 2))
+                                                               (bold ctx)))
+                                                        new-contexts))
+                  :always (conj (str (indent (+ 2 (count contexts)))
+                                     (str (bullet-color bullet) " " (color message)))))}))
 
-(defn report-test! [m config]
-  (reset! !state (-report-test! m (cljs.test/get-current-env) @!state config)))
+(defn report! [m config]
+  (let [{:keys [new-state printouts]} (get-report m (cljs.test/get-current-env) @!state config)]
+    (reset! !state new-state)
+    (doseq [printout printouts]
+      (println printout))))
 
 (defmethod cljs.test/report [:cljs.test/default :begin-test-var] [_m]
   (swap! !state merge (select-keys initial-state [:contexts])))
@@ -57,26 +61,26 @@
 
 (def ^:private original-pass (get-method cljs.test/report [:cljs.test/default :pass]))
 (defmethod cljs.test/report [:cljs.test/default :pass] [m]
-  (report-test! m {:color gray
-                  :bullet "✓"
-                  :bullet-color green})
+  (report! m {:color gray
+              :bullet "✓"
+              :bullet-color green})
   (original-pass m))
 
 (def ^:private original-fail (get-method cljs.test/report [:cljs.test/default :fail]))
 (defmethod cljs.test/report [:cljs.test/default :fail] [m]
   (let [failure-printout (with-out-str (original-fail m))]
     (swap! !state update :failure-prints conj failure-printout))
-  (report-test! m {:color red
-                  :bullet (str (count (:failure-prints @!state)) ")")
-                  :bullet-color red}))
+  (report! m {:color red
+              :bullet (str (count (:failure-prints @!state)) ")")
+              :bullet-color red}))
 
 (def ^:private original-error (get-method cljs.test/report [:cljs.test/default :error]))
 (defmethod cljs.test/report [:cljs.test/default :error] [m]
   (let [error-printout (with-out-str (original-error m))]
     (swap! !state update :failure-prints conj error-printout))
-  (report-test! m {:color red
-                  :bullet (str (count (:failure-prints @!state)) ")")
-                  :bullet-color red}))
+  (report! m {:color red
+              :bullet (str (count (:failure-prints @!state)) ")")
+              :bullet-color red}))
 
 (defmethod cljs.test/report [:cljs.test/default :begin-test-var] [m]
   (println (str (indent 1) (default (:var m)))))
